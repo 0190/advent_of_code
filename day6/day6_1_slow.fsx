0e78038147b62d327052682f8b38358474af2dab
Masha Klimkin <maria@spectory.com>
2015-12-20 16:49:50 +0200

    write toggle for optimized version

--- day6/day6_1_slow.fsx
+++ day6/day6_1_slow.fsx
@@ -5,15 +5,20 @@ let field = seq<coord> []
 
 let turnOn (s: state) (c1 : coord, c2 : coord) =
   let lights = lightsInRectangle c1 c2
-  Seq.concat [s;lights] |> Seq.distinct
+  Seq.concat [s; lights] |> Seq.distinct
 
 
 let turnOff (s: state) (c1 : coord, c2 : coord) =
   s |> Seq.filter (fun (x,y) ->
     x < fst c1 || x > fst c2 || y < snd c1 || y > snd c2)
 
-//let toggle (s: state) (c1 : coord, c2 : coord) =
-  //switchSome s (c1, c2) "toggle"
+let toggle (s: state) (c1 : coord, c2 : coord) =
+  //check this function
+  let toTurnOff = s |> Seq.filter (fun (x,y) ->
+    x >= fst c1 && x <= fst c2 && y >= snd c1 && y <= snd c2)
+  let toTurnOn = (lightsInRectangle c1 c2) |> Seq.filter (fun e -> Seq.exists ((=) e) toTurnOff)
+  Seq.concat [turnOff (s: state) (c1: coord, c2: coord); toTurnOn]
+
 
 
 let defaultParseCommand (line : string) =
