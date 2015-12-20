ef4c7905514954b65d6908258cfd22e895345780
Maria Klimkina <0910.nanami@gmail.com>
2015-12-20 19:16:06 +0200

    write toggle (to test)

--- day6/day6_1_slow.fsx
+++ day6/day6_1_slow.fsx
@@ -16,10 +16,8 @@ let toggle (s: state) (c1 : coord, c2 : coord) =
   //check this function
   let toTurnOff = s |> Seq.filter (fun (x,y) ->
     x >= fst c1 && x <= fst c2 && y >= snd c1 && y <= snd c2)
-  let toTurnOn = (lightsInRectangle c1 c2) |> Seq.filter (fun e -> Seq.exists ((=) e) toTurnOff)
-  Seq.concat [turnOff (s: state) (c1: coord, c2: coord); toTurnOn]
-
-
+  let toTurnOn = (lightsInRectangle c1 c2) |> Seq.filter (fun e -> (not (Seq.exists ((=) e) toTurnOff)))
+  Seq.concat [turnOff s (c1, c2); toTurnOn]
 
 let defaultParseCommand (line : string) =
   parseCommand turnOn turnOff turnOff line // add toggle
@@ -34,5 +32,5 @@ let run filePath parser init =
     | e :: l' -> helper l' (runOnField e f)
   helper (filePath |> readLines |> Seq.map parser |> Seq.toList) init
 
-//let answer = run "day6_tests.txt" defaultParseCommand field
-let answer = run fileName defaultParseCommand field
+//let answer = run fileName defaultParseCommand field
+let answer = run "day6_tests.txt" defaultParseCommand field
