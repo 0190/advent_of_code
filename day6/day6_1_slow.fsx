c8ac8055a317c9e8e32affa32403dd6288d4cbb1
Maria Klimkina <0910.nanami@gmail.com>
2015-12-20 22:18:37 +0200

    add a few comments

--- day6/day6_1_slow.fsx
+++ day6/day6_1_slow.fsx
@@ -13,11 +13,12 @@ let turnOff (s: state) (c1 : coord, c2 : coord) =
     x < fst c1 || x > fst c2 || y < snd c1 || y > snd c2)
 
 let toggle (s: state) (c1 : coord, c2 : coord) =
-  //check this function
   let toTurnOff = s |> Seq.filter (fun (x,y) ->
     x >= fst c1 && x <= fst c2 && y >= snd c1 && y <= snd c2)
+  // the slow part
   let toTurnOn = (lightsInRectangle c1 c2) |> Seq.filter (fun e -> (not (Seq.exists ((=) e) toTurnOff)))
-  Seq.concat [turnOff s (c1, c2); toTurnOn]
+  // the slow part end
+  Seq.concat [turnOff s (c1, c2); toTurnOff]
 
 let defaultParseCommand (line : string) =
   parseCommand turnOn turnOff toggle line // add toggle
@@ -32,5 +33,5 @@ let run filePath parser init =
     | e :: l' -> helper l' (runOnField e f)
   helper (filePath |> readLines |> Seq.map parser |> Seq.toList) init
 
-//let answer = run fileName defaultParseCommand field
-let answer = run "day6_tests.txt" defaultParseCommand field |> Seq.length
+//let answer = run "day6_tests.txt" defaultParseCommand field |> Seq.length
+let answer = run fileName defaultParseCommand field |> Seq.length
