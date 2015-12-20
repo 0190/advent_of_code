251e66d069077e3fd5709ac0519dc67650c8a57a
Maria Klimkina <0910.nanami@gmail.com>
2015-12-20 22:20:07 +0200

    remove a few files

--- day6/day6_1.fsx
+++ day6/day6_1.fsx
@@ -1,8 +1,37 @@
-module Day6_1
-type Coord = (int * int)
-type Segment = (Coord * Coord)
+#load "command_parser.fs"
+open CommandParser
 
-let mergeSegments ((x,y),(x',y'),(u,v),(u',v')) =
-  if x = u && x' = u' then [((x, min y v),(x', max y' v'))]
-  elif y = v && y' = v' then [((min x u, y),(max x' u', y'))]
-  else [(x,y),(x,y)] // else!
+let field = seq<coord> []
+
+let turnOn (s: state) (c1 : coord, c2 : coord) =
+  let lights = lightsInRectangle c1 c2
+  Seq.concat [s; lights] |> Seq.distinct
+
+
+let turnOff (s: state) (c1 : coord, c2 : coord) =
+  s |> Seq.filter (fun (x,y) ->
+    x < fst c1 || x > fst c2 || y < snd c1 || y > snd c2)
+
+let toggle (s: state) (c1 : coord, c2 : coord) =
+  let toTurnOff = s |> Seq.filter (fun (x,y) ->
+    x >= fst c1 && x <= fst c2 && y >= snd c1 && y <= snd c2)
+  // the slow part
+  let toTurnOn = (lightsInRectangle c1 c2) |> Seq.filter (fun e -> (not (Seq.exists ((=) e) toTurnOff)))
+  // the slow part end
+  Seq.concat [turnOff s (c1, c2); toTurnOff]
+
+let defaultParseCommand (line : string) =
+  parseCommand turnOn turnOff toggle line // add toggle
+
+let runOnField c f =
+  (fst c) f (snd c)
+
+let run filePath parser init =
+  let rec helper l f =
+    match l with
+    | [] -> f
+    | e :: l' -> helper l' (runOnField e f)
+  helper (filePath |> readLines |> Seq.map parser |> Seq.toList) init
+
+//let answer = run "day6_tests.txt" defaultParseCommand field |> Seq.length
+let answer = run fileName defaultParseCommand field |> Seq.length
